228034d290506ecd6a5be8a9c218e79f81ca2c1e
shahabyounas <younasshahab@gmail.com>
2020-05-08 18:47:38 +0400

    add site  menus

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Al Ghurair الغرير`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Shahab Younas`,
+    menuLinks:[
+            {
+               name:'Our Company',
+               link:'/'
+            },
+            {
+               name:'Our Sectors',
+               link:'/secotrs'
+            },
+            {
+              name: 'Media Center',
+              link: '/media'
+            },{
+              name:'Careers',
+              link: '/careers'
+            },
+          ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
